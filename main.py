fd6199efcecdbb86d15053bc96c9a5c38fe6b0f0
Brlesha <ikostin2002@mail.ru>
2023-09-10 02:11:20 +0300

    1

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ class Wallet():
     def person(self):
         return self.name, self.balance
 
+    def __add__(self, other):
+        if isinstance(other, Wallet):
+            new_title = 'Сумарный баланс счетов {s} и {o}'.format(s=self.name, o=other.name)
+            new_price = self.balance + other.balance
+        return new_title, new_price
+
+class RubleWallet(Wallet):
+    def __init__(self, name, balance, traderate=1):
 firstwallet = Wallet('MyWallet', 100, 1)
-print(firstwallet.person())
+secondwallet = Wallet('OtherWallet', 200, 1)
+print(firstwallet + secondwallet)
