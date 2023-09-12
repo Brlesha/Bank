facb97156568c385bc4d8936b256a166d07b6ed2
Brlesha <ikostin2002@mail.ru>
2023-09-13 02:43:26 +0300

    считает неправильно

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class Wallet():
     """Главный класс банка"""
+
     def __init__(self, name, balance):
         """Инициализация имени и баланса кошельков"""
         self.name = name
@@ -7,7 +8,7 @@ class Wallet():
 
     def __str__(self):
         """вывод объектов банка в виде имени и баланса кошелька"""
-        return f'{self.name}: {self.balance}'
+        return f'{self.type} {self.name}: {self.balance}'
 
     def __add__(self, other):
         """функция сложения объектов банка и сумм"""
@@ -16,11 +17,11 @@ class Wallet():
             if self.traderate <= other.traderate:
                 new_balance = self.balance + other.balance * other.traderate
             else:
-                new_balance = self.balance + other.balance/other.traderate
+                new_balance = self.balance + other.balance / self.traderate
         else:
             new_name = self.name
             new_balance = self.balance + float(other)
-        return "{t} {p} руб.".format(t=new_name, p=new_balance)
+        return "{t} {p} {c}".format(t=new_name, p=new_balance, c = self.currency)
 
     def __radd__(self, other):
         """функция сложения справа"""
@@ -59,19 +60,58 @@ class Wallet():
         new_balance = self.balance * self.traderate / float(other)
         return "{t} {p} руб.".format(t=new_name, p=new_balance)
 
+    def __eq__(self, other):
+        if isinstance(other, self.__class__) and self.balance * self.traderate == other.balance * other.traderate:
+            return True
+        else:
+            return False
+
+    def spend_all(self):
+        if self.balance > 0:
+            self.balance -= self.balance
+            return self
+        else:
+            print('баланс не положительный.')
+
+    def to_base(self):
+        if not isinstance(self, RubleWallet):
+            if self.traderate > 1:
+                self.balance = self.balance * self.traderate
+            else:
+                self.balance = self.balance / self.traderate
+        return self
+
+
+
+
 class RubleWallet(Wallet):
     """Рублевый кошелек банка"""
+    type = 'Ruble Wallet'
     traderate = 1
+    currency = 'руб.'
+
 
 class DollarWallet(Wallet):
     """Долларовый кошелек банка"""
+    type = 'Dollar Wallet'
     traderate = 60
+    currency = 'dlr.'
+
 
 class EuroWallet(Wallet):
     """Евро кошелек банка"""
+    type = 'Euro Wallet'
     traderate = 70
+    currency = 'eur.'
+
 
 firstwallet = DollarWallet('MyWallet', 6)
 secondwallet = RubleWallet('OtherWallet', 220)
 thirdwallet = EuroWallet('AnotherWallet', 1)
-print(2 * thirdwallet)''''''
+
+"""if EuroWallet('A', 2) == RubleWallet('B', 140):
+    print("Хайп")
+else:
+    print("Кринж")"""
+
+print(DollarWallet("Q", 150) + thirdwallet)
