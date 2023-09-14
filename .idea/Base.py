627f27a181bc0fff747355f576572e9e9d9e6f80
Brlesha Macbook <ikostin2002@mail.ru>
2023-09-14 11:21:17 +0300

    2

---
+++ .idea/Base.py
@@ -0,0 +1,10 @@
+class Field():
+    """Таблица типо ексель"""
+
+    def __init__(self, line_num, column_num, value):
+        """инициализация класса таблицы"""
+        self.line_num = line_num
+        self.column_num = column_num
+        self.value = value
+
+    def
