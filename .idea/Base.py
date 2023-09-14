663824569b497f74f4a9422a830bb460814fab7b
Brlesha Macbook <ikostin2002@mail.ru>
2023-09-14 13:50:22 +0300

    Добавляю таблицу 1

--- .idea/Base.py
+++ .idea/Base.py
@@ -1,10 +1,28 @@
-class Field():
+class table():
     """Таблица типо ексель"""
-
-    def __init__(self, line_num, column_num, value):
-        """инициализация класса таблицы"""
-        self.line_num = line_num
-        self.column_num = column_num
+    def __init__(self, index, value):
+        """инициализация ячейки таблицы"""
+        self.index = frozenset(index)
         self.value = value
 
-    def
+    # def write(self):
+    #     line = input('введите номер строки')
+    #     print(type(line))
+    #         if line.type = 'inte'
+    #         cloumn = str(input('введите индекс столбца'))
+    #
+    #
+    # def __getitem__(self, item):
+    #     if item in self:
+    #         return self.value
+    #     else:
+    #         return print('Эта ячейка пуста')
+
+
+# action = 'запись'
+# while action != 'Закончить':
+#     action = input('введите дейтсвие: ')
+#     if action == 'запись':
+#         table.write()
+
+a = input('Negro')
