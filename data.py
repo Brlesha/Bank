a7bf415ed353a3edc3f4db931e094643cfa8323f
Brlesha <ikostin2002@mail.ru>
2023-09-15 02:31:05 +0300

    Заново добавил таблу

---
+++ data.py
@@ -0,0 +1,28 @@
+class table():
+    """Таблица типо ексель"""
+    def __init__(self, index, value):
+        """инициализация ячейки таблицы"""
+        self.index = frozenset(index)
+        self.value = value
+
+    def write(self):
+        line = input('введите номер строки')
+        print(type(line))
+            if line.type = 'inte'
+            cloumn = str(input('введите индекс столбца'))
+
+
+    def __getitem__(self, item):
+        if item in self:
+            return self.value
+        else:
+            return print('Эта ячейка пуста')
+
+
+action = 'запись'
+while action != 'Закончить':
+    action = input('введите дейтсвие: ')
+    if action == 'запись':
+        table.write()
+
+a = input('Negro')
